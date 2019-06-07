5a237712860a5c86149aadad03b4218591ded47f
Andrew Perry <49868498+andperry256@users.noreply.github.com>
2019-06-07 10:51:50 +0100

    Add new files

---
+++ dbadmin/additional_header_lines.php
@@ -0,0 +1,8 @@
+<?php
+  print("<link rel='stylesheet' id='dbadmin-styles-css'  href='$BaseURL/_link_to_common/dbadmin/styles.css?v=$link_version' type='text/css' media='all' />\n");
+  print("<link rel='stylesheet' type=\"text/css\" href=\"$DBAdminURL/datepicker/css/jquery.datepick.css?v=$link_version\">\n");
+  print("<script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\"></script>\n");
+  print("<script type=\"text/javascript\" src=\"$DBAdminURL/datepicker/js/jquery.plugin.js\"></script>\n");
+  print("<script type=\"text/javascript\" src=\"$DBAdminURL/datepicker/js/jquery.datepick.js\"></script>\n");
+  print("<script type=\"text/javascript\" src=\"$DBAdminURL/datepicker/js/jquery.datepick-en-GB.js\"></script>\n");
+?>

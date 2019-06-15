162c3759c2364b24bec7620948a1a5f91ec1fb86
Andrew Perry <49868498+andperry256@users.noreply.github.com>
2019-06-15 14:56:43 +0100

    Changes to accommodate database mode 'auto'

--- dbadmin/database_sync.php
+++ dbadmin/database_sync.php
@@ -5,37 +5,68 @@ if (!function_exists('sync_databases'))
 //==============================================================================
 function sync_databases($local_db_name)
 {
-		global $Location, $RelativePath, $local_site_dir, $Localhost_ID;
+	global $Location, $RelativePath, $local_site_dir, $Localhost_ID, $DBAdminURL;
 
-		set_time_limit(300);
-		print("<h1>Synchronise Databases</h1>\n");
-		print("<h2 style=\"margin-bottom:0.5em\">($local_site_dir/$RelativePath)</h2>\n");
+	set_time_limit(300);
+	print("<h1>Synchronise Databases</h1>\n");
+	print("<h2 style=\"margin-bottom:0.5em\">($local_site_dir/$RelativePath)</h2>\n");
 
-		if ($Location == 'local')
+	if ($Location == 'local')
+	{
+		$sync_direction = 'in';  // Default direction
+		$db_sites = sites_db_connect();
+		$query_result = mysqli_query($db_sites,"SELECT * FROM dbases WHERE dbname='$local_db_name'");
+		if ($row = mysqli_fetch_assoc($query_result))
 		{
-			$db_sites = sites_db_connect();
-			$query_result = mysqli_query($db_sites,"SELECT * FROM dbases WHERE dbname='$local_db_name'");
-			if (($row = mysqli_fetch_assoc($query_result)) && ($row['mode'] == 'master'))
-				$sync_direction = 'out';
-			else
-				$sync_direction = 'in';
+			switch ($row['mode'])
+			{
+					case 'auto':
+						copy("$DBAdminURL/dbmode.php?site=$local_site_dir&dbname=$local_db_name","__temp_.txt");
+						$mode = trim(file_get_contents("__temp_.txt"));
+						unlink("__temp_.txt");
+						if ($mode == 'master')
+						{
+							$sync_direction = 'out';
+						}
+						elseif ($mode == 'sub-master')
+						{
+							$sync_direction = 'in';
+						}
+						break;
+
+					case 'master':
+						$sync_direction = 'out';
+						break;
+
+					case 'sub-master':
+						$sync_direction = 'in';
+						break;
+			}
 
 			if (isset($_POST['submitted']))
 			{
-				$cmd = "/Utilities/php_script mysql_sync $local_site_dir $RelativePath";
+				$cmd = "/Utilities/php_script mysql_sync $local_site_dir {$row['sub_path']}";
 				if ($_POST['sync_mode'] == 'backup')
+				{
 					$cmd .= " -b $Localhost_ID";
+				}
 				elseif ($_POST['sync_mode'] == 'restore')
+				{
 					$cmd .= " -r $Localhost_ID";
+				}
 				elseif ($_POST['sync_mode'] == $sync_direction)
+				{
 					$cmd .= " -s -force";
+				}
 				else
+				{
 					$cmd .= " -rs -force";
-				exec("$cmd > '__temp_.txt'");
-				$output = implode(file('__temp_.txt'));
+				}
+				exec("$cmd > '__temp2_.txt'");
+				$output = implode(file('__temp2_.txt'));
 				$output = str_replace("\n","<br />\n",$output);
-				print($output);
-				unlink ('__temp_.txt');
+				print("$output");
+				unlink ('__temp2_.txt');
 			}
 			else
 			{
@@ -44,12 +75,16 @@ function sync_databases($local_db_name)
 				print("<table cellpadding=\"8\">\n");
 				print("<tr><td><input type=\"radio\" name=\"sync_mode\" value=\"in\"");
 				if ($sync_direction == 'in')
+				{
 					print(" checked");
+				}
 				print("></td><td>Download online DB to local DB</td></tr>\n");
 
 				print("<tr><td><input type=\"radio\" name=\"sync_mode\" value=\"out\"");
 				if ($sync_direction == 'out')
+				{
 					print(" checked");
+				}
 				print("></td><td>Upload local DB to online DB</td></tr>\n");
 				print("<tr><td><input type=\"radio\" name=\"sync_mode\" value=\"backup\"></td><td>Back up local DB (locally)</td></tr>\n");
 				print("<tr><td><input type=\"radio\" name=\"sync_mode\" value=\"restore\"></td><td>Restore local DB (locally)</td></tr>\n");
@@ -60,7 +95,14 @@ function sync_databases($local_db_name)
 			}
 		}
 		else
-			print("<p>This facility is currently only supported on the local server.</p>\n");
+		{
+			print("<p>Unable to locate database record in sites database.</p>\n");
+		}
+	}
+	else
+	{
+		print("<p>This facility is currently only supported on the local server.</p>\n");
+	}
 }
 //==============================================================================
 }

5a7a5bf20ebc28dfecb7d2d05daa3682c56ce775
Andrew Perry <andy2@andperry.com>
2020-12-03 17:25:51 +0000

    Change to handle date input from date widget

--- dbadmin/finance_db_scripts/go_to_date_2.php
+++ dbadmin/finance_db_scripts/go_to_date_2.php
@@ -13,64 +13,30 @@ require_once("$PrivateScriptsDir/mysql_connect.php");
 require("$CustomPagesPath/$RelativePath/db_funct.php");
 $db = admin_db_connect();
 $table = $_GET['table'];
-$account = $_GET['account'];
+
+$query_result = mysqli_query($db,"SELECT * FROM dba_table_info WHERE table_name='transactions'");
+if ($row = mysqli_fetch_assoc($query_result))
+{
+  $list_size = $row['list_size'];
+}
+else
+{
+  exit("ERROR - Failed to read table info from DB - this should not occur!!");
+}
 
 if (isset($_POST['submitted']))
 {
-	// Validate/determine the required date.
-	if (empty($_POST['yr']))
+  if (!DateIsValid($_POST['date_selection']))
 	{
-		$error_message = "Invalid Date";
+    print("<p>Invalid Date</p>\n");
+    print("<p><a href=\"{$_GET['-returnurl']}\">Try again</a></p>\n");
 	}
-	else
-	{
-		if (empty($_POST['mth']))
-		{
-			if (!empty($_POST['dy']))
-			{
-				$error_message = "Invalid Date";
-			}
-			else
-			{
-				// No day & month specified. Set date to end of given year.
-				$_POST['mth'] = 12;
-			}
-		}
-		if (!empty($_POST['mth']))
-		{
-			if (empty($_POST['dy']))
-			{
-				// No day specified. Set date to end of given month.
-				$_POST['dy'] = DaysInMonth($_POST['mth'],$_POST['yr']);
-			}
-			elseif ($_POST['dy'] > DaysInMonth($_POST['mth'],$_POST['yr']))
-			{
-				$error_message = "Invalid Date";
-        print("<button onclick=\"goBack()\">Go Back</button>");
-		  }
-    }
-	}
-
-	if (isset($error_message))
-  {
-    print("<p>$error_message</p>\n");
-    print("<p><a href=\"\">Try again</a></p>\n");
-  }
   else
 	{
-		$display_date = sprintf("%04d-%02d-%02d",$_POST['yr'],$_POST['mth'],$_POST['dy']);
-		$query_result = mysqli_query($db,"SELECT * FROM $table WHERE date>'$display_date'");
+		$query_result = mysqli_query($db,"SELECT * FROM $table WHERE date>'{$_POST['date_selection']}'");
 		$display_offset = mysqli_num_rows($query_result);
-		if ($display_offset == 0)
-		{
-			$new_url = "$BaseURL/$RelativePath/?-table=$table";
-		}
-		elseif ($row = mysqli_fetch_assoc($query_result))
-		{
-			$display_offset = floor($display_offset/100) * 100;
-			$new_url = "$BaseURL/$RelativePath/?-table=$table&-startoffset=$display_offset";
-		}
-    header("Location: $new_url");
+    $display_offset = floor($display_offset/$list_size) * $list_size;
+    header("Location: $BaseURL/$RelativePath/?-table=$table&-startoffset=$display_offset");
     exit;
 	}
 }

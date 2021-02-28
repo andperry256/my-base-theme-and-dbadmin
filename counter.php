78507d596243eb7cea5f119ec0bc0df6b54e24cc
Andrew Perry <andy2@andperry.com>
2021-02-28 09:20:11 +0000

    Implement new counter mechanism
    The new mechanism stores all counter data in database records rather 
    than text files as before.

--- counter.php
+++ counter.php
@@ -1,18 +1,38 @@
 <?php
+
 	//==============================================================================
-	//    The following variables are defined in path_defs.php:-
-	//
-	//    $CounterStartMonth (mandatory) - month (integer) in which new year starts.
-	//    $CounterDir (mandatory) - path to directory containing counter file(s).
-	//    $CounterStartDate (optional) - original start date of counter if part
-	//                                   way through a normal counter year.
-	//    $DisplayCounter (optional) - indicates if counter is to be displayed
-	//                                 on page.
-	//
-	//    The variable $dbid must be defined in the calling script
+	/*
+    The following variables are pre-defined as required:-
+
+		$dbid (mandatory) - Database ID for the 'db_connect' function
+    $DisplayCounter (optional) - Indicates that the counter is to be displayed
+                                 on the page.
+		$StandaloneCounter (optional) - Indicates that the counter is being
+		                                displayed on the "web site index" page
+																		rather than its native website.
+		$MultilingualDates (optional) - Indicates that a multi-lingual version of
+		                                the 'ShortMonthName' function is in use.
+	*/
 	//==============================================================================
 
-	// Determine if remote user is likely to be a search engine / robot
+	if (!function_exists('DayName'))
+	{
+		require("$BaseDir/_link_to_common/date_funct.php");
+	}
+	if (!function_exists('db_connect'))
+	{
+		require("$PrivateScriptsDir/mysql_connect.php");
+	}
+	if (function_exists('db_connect_with_params'))
+	{
+		$db = db_connect_with_params($dbid,$DBMode,$Location);
+	}
+	else
+	{
+		$db = db_connect($dbid);
+	}
+
+	// Determine if the remote user is likely to be a search engine / robot.
 	$bot_identifiers = array(
     'bot',
     'slurp',
@@ -36,113 +56,105 @@
 	  }
 	}
 
-	if (!function_exists('DayName'))
+	// Determine the current counter year.
+	$this_year = (int)date('Y');
+	$this_month = (int)date('m');
+	$query_result = mysqli_query($db,"SELECT * FROM counter_info WHERE id='start_month'");
+	if ($row = mysqli_fetch_assoc($query_result))
 	{
-		require("$BaseDir/_link_to_common/date_funct.php");
-	}
-	if (!function_exists('db_connect'))
-	{
-		require("$PrivateScriptsDir/mysql_connect.php");
-	}
-
-	// Determine current counter year
-	$year = (int)date('Y');
-	$month = (int)date('m');
-	if ($month >= $CounterStartMonth)
-	{
-		$start_year = $year;
+		$start_month = (int)$row['value'];
+		if ($this_month >= $start_month)
+		{
+			$start_year = $this_year;
+		}
+		else
+		{
+			$start_year = $this_year - 1;
+		}
+		if ($start_month == 1)
+		{
+			$end_year = $start_year;
+		}
+		else
+		{
+			$end_year = $start_year + 1;
+		}
 	}
 	else
 	{
-		$start_year = $year - 1;
+		// This should not occur.
+		exit;
 	}
-	if ($CounterStartMonth == 1)
+
+	// Check if a counter exists for the current year.
+	$query_result = mysqli_query($db,"SELECT * FROM counter_info WHERE id='$end_year"."_count'");
+	if (mysqli_num_rows($query_result) == 0)
 	{
-		$end_year = $start_year;
+		// New counter year - add the required initialised records.
+		mysqli_query($db,"INSERT INTO counter_info (id,value) VALUES ('$end_year"."_count','0')");
+		mysqli_query($db,"INSERT INTO counter_info (id,value) VALUES ('$end_year"."_daily','0.0')");
+		$start_date = $start_year.'-'.sprintf("%02d",$start_month).'-01';
+		mysqli_query($db,"INSERT INTO counter_info (id,value) VALUES ('$end_year"."_start','$start_date')");
+	}
+
+	// Obtain the current counter start date.
+	$query_result = mysqli_query($db,"SELECT * FROM counter_info WHERE id='$end_year"."_start'");
+	if ($row = mysqli_fetch_assoc($query_result))
+	{
+		$counter_start_date = $row['value'];
 	}
 	else
 	{
-		$end_year = $start_year + 1;
+		// This should not occur.
+		exit;
 	}
 
-	$counter_file = "$CounterDir/counter-$end_year.txt";
-	$daily_average_file = "$CounterDir/daily-average-$end_year.txt";
+	$start_year = (int)substr($counter_start_date,0,4);
+	$start_month = (int)substr($counter_start_date,5,2);
+	$start_day = (int)substr($counter_start_date,8,2);
 	$today_date = date('Y-m-d');
-	if (function_exists('db_connect_with_params'))
+
+	// Read the counter for the current year.
+	$query_result = mysqli_query($db,"SELECT * FROM counter_info WHERE id='$end_year"."_count'");
+	if ($row = mysqli_fetch_assoc($query_result))
 	{
-		$db = db_connect_with_params($dbid,$DBMode,$Location);
+		$counter_val = (int)$row['value'];
 	}
 	else
 	{
-		$db = db_connect($dbid);
+		// This should not occur.
+		exit;
 	}
 
-	// Check if counter text file exists. If not create one and initialize it to zero.
-	if (!is_dir($CounterDir))
-	{
-		mkdir($CounterDir);
-	}
-	if (!file_exists($counter_file))
-	{
-	  $f = fopen($counter_file, "w");
-	  fwrite($f,"0");
-	  fclose($f);
-	}
-
-	// Read the current value from the counter file
-	$f = fopen($counter_file,"r");
-	$counter_val = fread($f, filesize($counter_file));
-	if (empty($counter_val))
-	{
-		$counter_val = 0;
-	}
-	fclose($f);
-
-	if ((!isset($StandaloneCounter)) && (!$is_bot))
+	if ((!$StandaloneCounter) && (!$is_bot))
 	{
 		$ip_addr = $_SERVER['REMOTE_ADDR'];
 		mysqli_query($db,"DELETE FROM counter_hits WHERE date<'$today_date'");
 		$query_result = mysqli_query($db,"SELECT * FROM counter_hits WHERE date='$today_date' AND ip_addr='$ip_addr'");
 		if (mysqli_num_rows($query_result) == 0)
 		{
+			// First visit of the day for the given client, so update the counter
 			$counter_val++;
-			$f = fopen($counter_file, "w");
-			fwrite($f, $counter_val);
-			fclose($f);
+			mysqli_query($db,"UPDATE counter_info SET value='$counter_val' WHERE id='$end_year"."_count'");
 			mysqli_query($db,"INSERT INTO counter_hits VALUES('$today_date','$ip_addr',$counter_val)");
 		}
 		elseif ($row = mysqli_fetch_assoc($query_result))
 		{
+			// Client has already visited today.
 			$own_counter = $row['count'];
 		}
 	}
 
-	// Determine start date of current count period
-	$year_start_date = sprintf("%04d-%02d-01",$start_year,$CounterStartMonth);
-	if ((isset($CounterStartDate)) && ($CounterStartDate > $year_start_date))
-	{
-		$start_year = substr($CounterStartDate,0,4);
-		$start_month = (int)substr($CounterStartDate,5,2);
-		$start_day = (int)substr($CounterStartDate,8,2);
-	}
-	else
-	{
-		$start_year = $start_year;
-		$start_month = $CounterStartMonth;
-		$start_day = 1;
-	}
-
-	// Calculate daily average
+	// Calculate and update the daily average for the current counting period.
 	$start_date_offset = mktime(1,0,0,$start_month,$start_day,$start_year);
 	$today_date_offset = mktime(1,0,0,(int)date('m'),(int)date('d'),(int)date('Y'));
 	$days_counting = (($today_date_offset - $start_date_offset) / 86400) + 1;
 	$daily_average = sprintf("%01.1f",$counter_val/$days_counting);
-	$f = fopen($daily_average_file, "w");
-	fwrite($f,"$daily_average");
-	fclose($f);
+	mysqli_query($db,"UPDATE counter_info SET value='$daily_average' WHERE id='$end_year"."_daily'");
 
-	if (isset($StandaloneCounter))
+	if ($StandaloneCounter)
 	{
+		// Generate 'standalone counter' display (i.e. not on native web page).
 		$query_result = mysqli_query($db,"SELECT * FROM counter_hits WHERE date='$today_date'");
 		$today_count = mysqli_num_rows($query_result);
 		print("<style>\n");
@@ -157,16 +169,16 @@
 	}
 	else
 	{
-		if (((isset($DisplayCounter)) && ($DisplayCounter === true)) || (isset($_GET['showcount'])))
+		if (($DisplayCounter) || (isset($_GET['showcount'])))
 		{
-			// Display counter on web page
+			// Generate normal counter display (i.e. on native web page).
 			print("You are visitor number ");
 			if (isset($own_counter))
 			{
 				$counter_val = $own_counter;
 			}
 			print(sprintf("<span class=\"counter\">&nbsp;%05d&nbsp;</span>",$counter_val));
-			if (isset($MultilingualDates))
+			if ($MultilingualDates)
 			{
 				print(sprintf("<br />since %02d %s $start_year",$start_day, MonthName($start_month,'en')));
 			}
@@ -177,7 +189,7 @@
 		}
 		if (isset($_GET['showcount']))
 		{
-			// Display daily average on web page
+			// Add daily average to the display.
 			print(sprintf("<br/>Average %01.1f counts per day",$counter_val/$days_counting));
 		}
 	}

fbf2160297d836bfb5b582d10e0a1398e72901a8
Andrew Perry <andy2@andperry.com>
2021-06-14 10:55:02 +0100

    Add constant definitions
    Patterns for regex matches on variables in relationship queries.

--- dbadmin/functions.php
+++ dbadmin/functions.php
@@ -1,4 +1,10 @@
 <?php
+	if (!defined('RELATIONSHIP_VARIABLE_MATCH_1'))
+	{
+		$dummy = '({';  // To avoid false positives in PHP code checker
+		DEFINE('RELATIONSHIP_VARIABLE_MATCH_1','/[ =<>*+\'\^\)\}]\$[A-Za-z0-9_]+/');
+		DEFINE('RELATIONSHIP_VARIABLE_MATCH_2',str_replace('\\$','\\$\\$',RELATIONSHIP_VARIABLE_MATCH_1));
+	}
 	require("update_table_data.php");
 	require("common_funct.php");
 	require("view_funct.php");

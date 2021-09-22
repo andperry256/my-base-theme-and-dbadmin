9f6d14002aa4ecd9d550935affe4a36cc927be34
Andrew Perry <andy2@andperry.com>
2021-09-22 11:17:05 +0100

    Add security check at top of script

--- common_scripts/display_debug_log.php
+++ common_scripts/display_debug_log.php
@@ -1,4 +1,8 @@
 <?php
+  if (($_SERVER['REMOTE_ADDR'] != '212.159.74.141') && (substr($_SERVER['REMOTE_ADDR'],0,10) != '192.168.0.'))
+  {
+    exit("Authentication failure");
+  }
   if (isset($_GET['site']))
   {
     $local_site_dir = $_GET['site'];

ab60d62a735f2158f6e4096a96c39633b3fe5179
Andrew Perry <andy2@andperry.com>
2021-10-06 16:12:32 +0100

    Set the value of each element to the server description

--- common_scripts/allowed_hosts.php
+++ common_scripts/allowed_hosts.php
@@ -1,7 +1,9 @@
 <?php
   $allowed_hosts = array (
-  	'212.159.74.141' => true,  // Home
-  	'91.238.160.174' => true,  // HostPresto
-  	'54.191.137.17' => true,   // ManageWP
+    '127.0.0.1' => 'Localhost',
+  	'212.159.74.141' => 'Home',
+    '217.45.173.179' => 'Longcroft',
+  	'91.238.160.174' => 'HostPresto',
+  	'54.191.137.17' => 'ManageWP',
   );
 ?>

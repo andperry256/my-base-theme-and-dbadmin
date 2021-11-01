c7fef8ed6cd80afd9b6cdb0fe22ec7bab05daeff
Andrew Perry <andy2@andperry.com>
2021-11-01 12:09:34 +0000

    Various changes
    1. Include functions moved out from the theme functions.php.
    2. Include handling of light/dark theme scripts in include_inline_stylesheet function.
    3. Add various program comments.

--- wp-content/themes/my-base-theme/shared/functions.php
+++ wp-content/themes/my-base-theme/shared/functions.php
@@ -6,7 +6,7 @@
  * Additional functions that may need to be accessed by scripts running
  * outside the WordPress environment.
  */
- //================================================================================
+//================================================================================
 
 if (!function_exists('run_session'))
 {
@@ -174,6 +174,23 @@ function run_session()
 	}
 }
 
+//================================================================================
+/*
+ * Session variable handling functions
+ *
+ * This section includes the following functions:-
+ * 1. session_var_is_set
+ * 2. get_session_var
+ * 3. update_session_var
+ * 4. delete_session_var
+ *
+ * Each function determines whether actual $_SESSION variables are in force
+ * or whether they have been saved to $GlobalSessionVars and the PHP session
+ * closed.
+ *
+ * Each function takes a 'name' parameter for the session variable name, plus
+ * an optional 'name2' variable for 2-dimensional array handling.
+ */
 //================================================================================
 
 function session_var_is_set($name,$name2='')
@@ -386,14 +403,281 @@ function delete_session_var($name,$name2='')
 }
 
 //================================================================================
+/*
+ * Function set_default_header_image_paths
+ */
+//================================================================================
+
+function set_default_header_image_paths()
+{
+	$image_file_exts = array( 'png', 'jpg' );
+	global $desktop_header_image_path;
+	global $desktop_header_image_url;
+	global $intermediate_header_image_path;
+	global $intermediate_header_image_url;
+	global $mobile_header_image_path;
+	global $mobile_header_image_url;
+	$current_theme_dir = get_stylesheet_directory();
+	$current_theme_url = get_stylesheet_directory_uri();
+
+	$desktop_header_image_path = '';
+	$desktop_header_image_url = '';
+	foreach ($image_file_exts as $ext)
+	{
+		if (is_file("$current_theme_dir/header_image.$ext"))
+		{
+			$desktop_header_image_path = "$current_theme_dir/header_image.$ext";
+			$desktop_header_image_url = "$current_theme_url/header_image.$ext";
+			break;
+		}
+	}
+
+	$intermediate_header_image_path = $desktop_header_image_path;
+	$intermediate_header_image_url = $desktop_header_image_url;
+	foreach ($image_file_exts as $ext)
+	{
+		if (is_file("$current_theme_dir/header_image_intermediate.$ext"))
+		{
+			$intermediate_header_image_path = "$current_theme_dir/header_image_intermediate.$ext";
+			$intermediate_header_image_url = "$current_theme_url/header_image_intermediate.$ext";
+			break;
+		}
+	}
+
+	$mobile_header_image_path = $intermediate_header_image_path;
+	$mobile_header_image_url = $intermediate_header_image_url;
+	foreach ($image_file_exts as $ext)
+	{
+		if (is_file("$current_theme_dir/header_image_mobile.$ext"))
+		{
+			$mobile_header_image_path = "$current_theme_dir/header_image_mobile.$ext";
+			$mobile_header_image_url = "$current_theme_url/header_image_mobile.$ext";
+			break;
+		}
+	}
+}
+
+//================================================================================
+/*
+ * Function set_header_image_paths
+ */
+//================================================================================
+
+function set_header_image_paths($slug,$type)
+{
+	if (function_exists('set_custom_header_image_paths'))
+	{
+		// Call child theme function
+		set_custom_header_image_paths($slug,$type);
+	}
+	else
+	{
+		// No action - the default paths will apply
+	}
+}
+
+//================================================================================
+/*
+ * Function output_page_header
+ *
+ * This function is used to output the main title header of the current page.
+ * It is dependent upon the installation of the 'Secondary Title' plugin.
+ */
+//================================================================================
+
+function output_page_header()
+{
+	if (function_exists('get_secondary_title'))
+		$secondary_title = get_secondary_title();
+	else
+		$secondary_title = '';
+	if ($secondary_title == '#')
+	{
+		// No action
+	}
+	elseif (!empty($secondary_title))
+		echo("<h1>$secondary_title</h1>\n");
+	else
+		the_title( '<h1 class="entry-title">', '</h1>' );
+}
+
+//================================================================================
+/*
+ * Function get_content_part
+ *
+ * This function is used extract and output a given portion of the page content
+ * and is for use when the content section of a page is being built using a custom
+ * PHP script. A numeric part number is passed as a paramaeter and this indicates
+ * that the text is to be extracted from between the following tags in the
+ * WordPress page content:-
+ *
+ * [part<n>]
+ * [/part<n>]
+ *
+ * where <n> is the part number. This allows multiple portions to be extracted
+ * from the pages content for use at different points in the page.
+ */
+//================================================================================
+
+function get_content_part($part_no,$option='')
+{
+	$page_id = get_the_ID();
+	$page_object = get_page($page_id);
+	$content = $page_object->post_content;
+	$dummy = "[[[[[[[[";  // To prevent false positive in PHP code checker
+	if ($part_no == 0)
+	{
+		// Use part number 0 to return whole page content
+		$content = apply_filters( 'the_content', $content );
+		$content = str_replace( ']]>', ']]&gt;', $content );
+	}
+	else
+	{
+		$pos1 = strpos($content,"[part$part_no]");
+		$pos2 = strpos($content,"[/part$part_no]");
+		if (($pos1 === false) || ($pos2 === false))
+			return "**** Unable to retrieve part $part_no from page ****";
+		$pos1 += strlen("[part$part_no]");
+		$content = substr($content,$pos1,$pos2-$pos1);
+		$content = apply_filters( 'the_content', $content );
+		$content = str_replace( ']]>', ']]&gt;', $content );
+		$content = str_replace( '__', '&nbsp;', $content );
+	}
+	if ($option == 'strip_paras')
+	{
+		$content = str_replace('<p>','',$content);
+		$content = str_replace('</p>','',$content);
+	}
+	return $content;
+}
+
+//================================================================================
+/*
+ * Function output_meta_data
+ *
+ * This function is used to generate meta tag data in the page header.
+ * A number of global variables are referenced by the function to set up the tags
+ * as required. These will have been set up previously by running any 'metadata.php'
+ * scripts in the page hierachy within the custom scripts folder.
+ *
+ * N.B. To cancel a meta description from an ancestor page without creating a new one,
+ * the meta description must be re-defined for the page as an empty string.
+ */
+//================================================================================
+
+function output_meta_data()
+{
+	global $meta_description;
+	global $meta_robots_noindex;
+	global $meta_robots_nofollow;
+	global $meta_refresh_interval;
+	global $meta_refresh_url;
+	global $meta_refresh_url_pars;
+	global $Location;
+
+	if ((isset($Location)) && ($Location == 'local'))
+		print("<meta name=\"robots\" content=\"noindex,nofollow\">\n");
+	else
+	{
+		if ((isset($meta_description)) && (!empty($meta_description)))
+		{
+			print("<meta name=\"description\" content=\"$meta_description\">\n");
+		}
+		$robots_content = '';
+		if ((isset($meta_robots_noindex)) && ($meta_robots_noindex))
+		{
+			$robots_content = 'noindex';
+		}
+		if ((isset($meta_robots_nofollow)) && ($meta_robots_nofollow))
+		{
+			if (!empty($robots_content))
+			{
+				$robots_content .= ',';
+			}
+			$robots_content .= 'nofollow';
+		}
+			if (!empty($robots_content))
+			{
+				print("<meta name=\"robots\" content=\"$robots_content\">\n");
+			}
+	}
+
+	if ((isset($meta_refresh_interval)) && (isset($meta_refresh_url)) && (!isset($_GET['norefresh'])))
+	{
+		if (!isset($meta_refresh_url_pars))
+		{
+			$meta_refresh_url_pars = '';
+		}
+		print("<meta http-equiv=\"refresh\" content=\"$meta_refresh_interval;URL='$meta_refresh_url/$meta_refresh_url_pars'\" />\n");
+	}
+}
+
+//================================================================================
+/*
+ * Function output_stylesheet_link
+ *
+ * This function is used to output a stylesheet link in the HTML header when
+ * the URL hierachy is scanned by setup_params.php. The stylesheet file must be
+ * named styles.css.
+ *
+ * The associated light/dark theme stylesheet will also be linked in if present.
+ */
+//================================================================================
+
+function output_stylesheet_link($path,$sub_path)
+{
+	global $link_version, $BaseDir, $BaseURL;
+	$stylesheet_id = str_replace('/','-',$sub_path);
+	$dir_path = str_replace($BaseURL,$BaseDir,$path);
+	print("\n<link rel='stylesheet' id='$stylesheet_id"."-styles-css'  href='$path/$sub_path/styles.css?v=$link_version' type='text/css' media='all' />\n");
+	if ((get_session_var('theme_mode') == 'light') && (is_file("$dir_path/$sub_path/styles-light.css")))
+	{
+		print("\n<link rel='stylesheet' id='$stylesheet_id"."-styles-light-css'  href='$path/$sub_path/styles-light.css?v=$link_version' type='text/css' media='all' />\n");
+	}
+	elseif ((get_session_var('theme_mode') == 'dark') && (is_file("$dir_path/$sub_path/styles-dark.css")))
+	{
+		print("\n<link rel='stylesheet' id='$stylesheet_id"."-styles-dark-css'  href='$path/$sub_path/styles-dark.css?v=$link_version' type='text/css' media='all' />\n");
+	}
+}
+
+//================================================================================
+/*
+ * Function include_inline_stylesheet
+ *
+ * This function loads a stylesheet file and outputs its contents within
+ * <style></style> tags by way of inline styles. It can be called from anywhere
+ * but is also used by setup_params.php when scanning the URL hierachy. There
+ * is no constraint on the stylesheet filename, but when called from
+ * setup_params.php, it will always be inline-styles.css.
+ *
+ * The associated light/dark theme stylesheet will also be included if present.
+ */
+//================================================================================
 
 function include_inline_stylesheet($path)
 {
 	print("<style>\n");
-	include($path);
+	if (is_file($path))
+	{
+		include($path);
+	}
+	$light_theme_path = str_replace('.css','-light.css',$path);
+	$dark_theme_path = str_replace('.css','-dark.css',$path);
+	if ((get_session_var('theme_mode') == 'light') && (is_file($light_theme_path)))
+	{
+		include($light_theme_path);
+	}
+	elseif ((get_session_var('theme_mode') == 'dark') && (is_file($dark_theme_path)))
+	{
+		include($dark_theme_path);
+	}
 	print("</style>\n");
 }
 
+//================================================================================
+/*
+ * Functions save_php_error_log & restore_php_error_log
+ */
 //================================================================================
 
 function save_php_error_log()
@@ -418,6 +702,10 @@ function restore_php_error_log()
 	}
 }
 
+//================================================================================
+/*
+ * Function output_to_access_log
+ */
 //================================================================================
 
 function output_to_access_log($user='',$add_info='')

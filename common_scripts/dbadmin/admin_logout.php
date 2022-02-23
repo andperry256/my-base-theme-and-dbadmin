c2ef2a425e9a562018d13fc87d0664a24dc170cb
Andrew Perry <andy2@andperry.com>
2022-02-23 12:33:26 +0000

    Replace $_SESSION['user'] with $_SESSION[SV_USER]

--- common_scripts/dbadmin/admin_logout.php
+++ common_scripts/dbadmin/admin_logout.php
@@ -6,11 +6,11 @@
 	require_once("{$_SERVER['DOCUMENT_ROOT']}/path_defs.php");
 
 	/*
-	Log the user off by clearing the username but leaving the $_SESSION['user']
+	Log the user off by clearing the username but leaving the $_SESSION[SV_USER]
 	variable intact. This enables the user to be kept logged off in the local
 	network enviroment, which may occasionally be required for testing purposes.
 	*/
-	$_SESSION['user'] = '';
+	$_SESSION[SV_USER] = '';
 	header("Location: $BaseURL");
 	exit;
 ?>

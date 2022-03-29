be9acb525bcab31e12cd2e42d465febe5027120a
Andrew Perry <andy2@andperry.com>
2022-03-29 13:52:56 +0100

    Add variable definitions
    So that $home_remote_ip_addr and $longcroft_remote_ip_addr are available as global variables.

--- common_scripts/allowed_hosts.php
+++ common_scripts/allowed_hosts.php
@@ -1,8 +1,10 @@
 <?php
+  $home_remote_ip_addr = '212.159.74.141';
+  $longcroft_remote_ip_addr = '217.45.173.179';
   $allowed_hosts = array (
     '127.0.0.1' => 'Localhost',
-  	'212.159.74.141' => 'Home',
-    '217.45.173.179' => 'Longcroft',
+  	$home_remote_ip_addr => 'Home',
+    $longcroft_remote_ip_addr => 'Longcroft',
   	'91.238.160.174' => 'HostPresto',
   	'54.191.137.17' => 'ManageWP',
   );

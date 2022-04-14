11ee7eafbf78fbb21f04cb3a35c975ca71f23ed5
Andrew Perry <andy2@andperry.com>
2022-04-14 11:48:21 +0100

    Remove function regex_match__validate
    The conditions checked for are now longer considered erroneous.

--- common_scripts/dbadmin/finance_db_scripts/tables/payees/payees.php
+++ common_scripts/dbadmin/finance_db_scripts/tables/payees/payees.php
@@ -2,16 +2,6 @@
 
 class tables_payees
 {
-	function regex_match__validate($record, $value)
-	{
-		if ((!empty($value)) & (strtoupper($value) != $value))
-		{
-			return report_error("Regex match patter must be all uppercase.");
-		}
-		else
-			return true;
-	}
-
 	function beforeSave($record)
 	{
 

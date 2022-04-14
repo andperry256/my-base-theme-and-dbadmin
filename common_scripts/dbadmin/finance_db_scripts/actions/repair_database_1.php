e5788e2242d4bc0d533fb44274ee9d9bcb87e574
Andrew Perry <andy2@andperry.com>
2022-04-14 11:46:54 +0100

    Rename options to 'Dry Run' and 'Live Run'.

--- common_scripts/dbadmin/finance_db_scripts/actions/repair_database_1.php
+++ common_scripts/dbadmin/finance_db_scripts/actions/repair_database_1.php
@@ -5,8 +5,8 @@ $db = admin_db_connect();
 
 print("<h1>Repair Database</h1>\n");
 
-print("<p><a href=\"index.php?-action=repair_database_2&preview\">Run in Preview Mode</a></p>\n");
-print("<p><a href=\"index.php?-action=repair_database_2\">Run in Real Mode</a></p>\n");
+print("<p><a href=\"index.php?-action=repair_database_2&dry-run\"><button>Dry Run</button></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+print("<a href=\"index.php?-action=repair_database_2\"><button>Live Run</button></a></p>\n");
 
 //==============================================================================
 ?>

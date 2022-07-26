34c411009681c4658cb0a89056031b004bc9ef0a
Andrew Perry <andy2@andperry.com>
2022-07-26 21:23:37 +0100

    Add handling of new sidebar styles in desktop mode
    So that the sidebar has a different background colour for the online site and a local server,

--- common_scripts/dbadmin/additional_header_lines.php
+++ common_scripts/dbadmin/additional_header_lines.php
@@ -3,9 +3,17 @@
   if (get_session_var('theme_mode') == 'dark')
   {
     print("<link rel='stylesheet' id='dbadmin-styles-dark-css'  href='$BaseURL/common_scripts/dbadmin/styles-dark.css?v=$link_version' type='text/css' media='all' />\n");
+    if ($Location == 'local')
+    {
+      print("<link rel='stylesheet' id='dbadmin-styles-local-dark.css'  href='$BaseURL/common_scripts/dbadmin/styles-local-dark.css?v=$link_version' type='text/css' media='all' />\n");
+    }
   }
   else
   {
     print("<link rel='stylesheet' id='dbadmin-styles-light-css'  href='$BaseURL/common_scripts/dbadmin/styles-light.css?v=$link_version' type='text/css' media='all' />\n");
+    if ($Location == 'local')
+    {
+      print("<link rel='stylesheet' id='dbadmin-styles-local-light.css'  href='$BaseURL/common_scripts/dbadmin/styles-local-light.css?v=$link_version' type='text/css' media='all' />\n");
+    }
   }
 ?>

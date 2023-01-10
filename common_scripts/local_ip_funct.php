55f827bd6e7c00b91eb79fbbebb74d6c7fd93acd
Andrew Perry <andy2@andperry.com>
2023-01-10 15:57:51 +0000

    Add program comments

--- common_scripts/local_ip_funct.php
+++ common_scripts/local_ip_funct.php
@@ -8,10 +8,13 @@ if (!function_exists('is_local_ip'))
 		global $IP_Subnet_Addr;
 		if (!isset($IP_Subnet_Addr))
 		{
+			// This will be the case for an online web site.
 			return false;
 		}
 		else
 		{
+			// Check if the first two elements of the IP address match those of
+			// the subnet address defined for the local network.
 			$tok1a = strtok($ip_addr,'.');
 			$tok2a = strtok('.');
 			$tok1b = strtok($IP_Subnet_Addr,'.');

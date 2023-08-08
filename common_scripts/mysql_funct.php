7cac62a44f430ad00b974f57f5bc1e0abe4212ed
Andrew Perry <andy2@andperry.com>
2023-08-08 13:12:30 +0100

    Move functions back into mysql_funct.php
    Deleting mysql_old_funct.php in the process.

--- common_scripts/mysql_funct.php
+++ common_scripts/mysql_funct.php
@@ -3,8 +3,6 @@
 if (!function_exists('run_prepared_statement'))
 {
 //==============================================================================
-require(__DIR__.'/mysql_old_funct.php');  // Temporary file inclusion
-//==============================================================================
 
 function print_stack_trace_for_mysqli_error($ofp=false)
 {
@@ -26,6 +24,89 @@ function print_stack_trace_for_mysqli_error($ofp=false)
 			fprintf($ofp,"  $line$eol");
 		}
 	}
+}
+
+//==============================================================================
+/*
+Function run_mysqli_query
+
+This is called in place of a regular call to mysqli_query and is used to output
+a more useful error message if the MySQL function call raises an exception.
+
+If the $strict option is set, then it will also abort with an error message if
+the MySQL function call runs without an exception but returns an error value.
+
+On an online server, errors are output to a log file rather than the screen.
+*/
+//==============================================================================
+
+function run_mysqli_query($db,$query,$strict=false)
+{
+	global $argc, $RootDir;
+	$eol = (isset($argc)) ? "\n" : "<br />\n";
+	$error_id = substr(md5(date('YmdHis')),0,8);
+	$date_and_time = date('Y-m-d H:i:s');
+	$fatal_error_message = "There has been a fatal error, details of which have been logged.$eol";
+	$fatal_error_message .= "Please report this to the webmaster quoting code <strong>$error_id</strong>.$eol";
+	try
+	{
+		$result = mysqli_query($db,$query);
+	}
+	catch (Exception $e)
+	{
+		if (is_file("/Config/linux_pathdefs.php"))
+		{
+			// Local server
+			print("Error caught on running MySQL query:$eol$query$eol");
+			print($e->getMessage().$eol);
+			print_stack_trace_for_mysqli_error();
+		}
+		else
+		{
+			// Online server
+			$ofp = fopen("$RootDir/logs/php_error.log",'a');
+			fprintf($ofp,"[$date_and_time] [$error_id] Error caught on running MySQL query:\n  $query\n");
+			fprintf($ofp,'  '.$e->getMessage()."\n");
+			fclose($ofp);
+			print_stack_trace_for_mysqli_error($ofp);
+			print($fatal_error_message);
+		}
+		exit;
+	}
+	if ((!$result) && ($strict))
+	{
+		if (is_file("/Config/linux_pathdefs.php"))
+		{
+			// Local server
+			print("Error result returned from MySQL query:$eol$query$eol");
+			print_stack_trace_for_mysqli_error();
+		}
+		else
+		{
+			// Online server
+			$ofp = fopen("$RootDir/logs/php_error.log",'a');
+			fprintf($ofp,"[$date_and_time] [$error_id] Error result returned from MySQL query:\n  $query\n");
+			fclose($ofp);
+			print_stack_trace_for_mysqli_error($ofp);
+			print($fatal_error_message);
+		}
+		exit;
+	}
+	return $result;
+}
+
+//==============================================================================
+
+function mysqli_query_normal($db,$query)
+{
+	return run_mysqli_query($db,$query,false);
+}
+
+//==============================================================================
+
+function mysqli_query_strict($db,$query)
+{
+	return run_mysqli_query($db,$query,true);
 }
 
 //==============================================================================

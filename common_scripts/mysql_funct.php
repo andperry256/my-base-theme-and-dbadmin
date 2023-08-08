f00706400a3b0b63a054b0433394e7da5648d57a
Andrew Perry <andy2@andperry.com>
2023-08-08 09:45:59 +0100

    Updates to program comments

--- common_scripts/mysql_funct.php
+++ common_scripts/mysql_funct.php
@@ -30,32 +30,25 @@ function print_stack_trace_for_mysqli_error($ofp=false)
 
 //==============================================================================
 /*
-Use of the $strict parameter:
-
-This parameter is included in all the following functions and is set to 'true'
-to indicate that even if the running of the query does not in itself raise an
-exception, the query result needs to be checked to determine if it is safe for
-the calling script to proceed to the next stage.
-
-It is set as an optional parameter in all cases where it is use. Normally it
-would be set 'true' for select queries and 'false' for most other queries (to
-be ratified).
-*/
-//==============================================================================
-/*
 Function run_prepared_statement
 
 This is called in place of a regular call to mysqli_stmt_execute and is used to
-output a more useful error message if the MySQL function call raises an exception.
+output a more useful error message if the MySQL function call raises an
+exception.
 
 If the $strict option is set, then it will also abort with an error message if
-the MySQL function call runs without an exception but returns an error value.
+the MySQL function call runs without an exception but returns a value that
+indicates a potential error condition making it unsafe for the calling script
+to continue execution. This parameter is passed by all calling functions and is
+contained within the call line to each of these as an optional parameter with a
+default (see functions below). Normally the $strict option would be set to
+'true' for select queries and 'false' for other query types (to be ratified).
 
 On an online server, errors are output to a log file rather than the screen.
 */
 //==============================================================================
 
-function run_prepared_statement($stmt,$strict=false)
+function run_prepared_statement($stmt,$strict)
 {
 	global $argc, $RootDir;
 	$eol = (isset($argc)) ? "\n" : "<br />\n";
@@ -128,7 +121,7 @@ $where_clause - WHERE clause to be used in the query (optional). Values to
 $where_values - Values associated with the WHERE clause (array).
 $add_clause - Any additional clause to be added to the query (opitional -
               includes for example ORDER or LIMIT direactive).
-$strict (optional) - See above.
+$strict (optional) - See run_prepared_statement function.
 
 The query result is returned.
 */
@@ -172,7 +165,7 @@ $set_values - Values associated with the field list (array)
 $where_clause - WHERE clause to be used in the query (optional). Values to
                 compare with are included as question marks.
 $where_values - Values associated with the WHERE clause (array).
-$strict (optional) - See above.
+$strict (optional) - See run_prepared_statement function.
 
 The query result is returned.
 */
@@ -217,7 +210,7 @@ $db - Link to the connected database
 $table - Associated table
 $fields - List of specified fields (comma separated string)
 $values - Values associated with the field list (array)
-$strict (optional) - See above.
+$strict (optional) - See run_prepared_statement function.
 
 The query result is returned.
 */
@@ -250,7 +243,7 @@ $table - Associated table
 $where_clause - WHERE clause to be used in the query. Values to compare with
                 are included as question marks.
 $where_values - Values associated with the WHERE clause (array).
-$strict (optional) - See above.
+$strict (optional) - See run_prepared_statement function.
 
 The query result is returned.
 */
@@ -287,7 +280,7 @@ The following parameters are passed:-
 $db - Link to the connected database
 $query - Query text,
 $where_values - Parameters to be bound.
-$strict (optional) - See above.
+$strict (optional) - See run_prepared_statement function.
 
 */
 //==============================================================================

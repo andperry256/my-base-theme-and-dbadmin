23e86a30ebc2d949248b2c1277369897b76497d2
Andrew Perry <andy2@andperry.com>
2023-08-23 09:10:43 +0100

    Make user friendly changes

--- common_scripts/mysql_funct_templates.php
+++ common_scripts/mysql_funct_templates.php
@@ -3,30 +3,32 @@
 
 // SELECT
 $where_clause = '';
-$where_values = array();
+$where_values = array('s',);
 $add_clause = '';
 $query_result = mysqli_select_query($db,'','*',$where_clause,$where_values,'');
+$query_result = mysqli_select_query($db,'','*','',array(),'');
 
 // UPDATE
 $set_fields = '';
-$set_values = array();
+$set_values = array('s',);
 $where_clause = '';
-$where_values = array();
+$where_values = array('s',);
 mysqli_update_query($db,'',$set_fields,$set_values,$where_clause,$where_values);
+mysqli_update_query($db,'',$set_fields,$set_values,'',array());
 
 // INSERT
 $fields = '';
-$values = array();
+$values = array('s',);
 mysqli_insert_query($db,'',$fields,$values);
 
 // DELETE
 $where_clause = '';
-$where_values = array();
+$where_values = array('s',);
 mysqli_delete_query($db,'',$where_clause,$where_values);
 
 // FREE FORMAT
 $query = "";
-$where_values = array();
+$where_values = array('s',);
 mysqli_free_format_query($db,$query,$where_values);
 
 //==============================================================================

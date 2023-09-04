13a0f95a878abade537be5cea9497b47dc09d488
Andrew Perry <andy2@andperry.com>
2023-09-04 16:39:57 +0100

    Add new file

---
+++ common_scripts/globals.php
@@ -0,0 +1,16 @@
+<?php
+//==============================================================================
+/*
+This script is included by the path_defs.php scripts on all sites. It declares
+a number of key variables as global.
+*/
+//==============================================================================
+
+global $Location,$RootDir,$DBMode;
+global $BaseDir,$BaseURL,$RelativePath;
+global $DBAdminDir,$DBAdminURL;
+global $CustomPagesPath,$CustomPagesURL;
+global $CustomScriptsPath,$CustomScriptsURL;
+
+//==============================================================================
+?>

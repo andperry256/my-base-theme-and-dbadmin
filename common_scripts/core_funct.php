75a20d15f9f70a400b2cbb369c92264c4660147f
Andrew Perry <andy2@andperry.com>
2023-09-09 10:15:07 +0100

    Add new files

---
+++ common_scripts/core_funct.php
@@ -0,0 +1,12 @@
+<?php
+  if ((isset($BaseDir)) && (isset($PrivateScriptsDir)))
+  {
+    require_once("$PrivateScriptsDir/mysql_connect.php");
+    require_once("$BaseDir/common_scripts/mysql_funct.php");
+    require_once("$BaseDir/common_scripts/session_funct.php");
+  }
+  else
+  {
+    exit("Directory paths not found\n");
+  }
+?>

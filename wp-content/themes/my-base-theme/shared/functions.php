6b692f13adf111d76cc4c1bfb5cbbe9bfc218d79
Andrew Perry <andy2@andperry.com>
2024-03-25 11:41:35 +0000

    Move function readable_markup

--- wp-content/themes/my-base-theme/shared/functions.php
+++ wp-content/themes/my-base-theme/shared/functions.php
@@ -391,23 +391,6 @@ function output_to_access_log($user='',$add_info='')
 
 //================================================================================
 /*
-* Function readable_markup
-*
-* This function is used to display markup code (HTML/XML) visibly in the
-* browser window when setting a debug point.
-*/
-//================================================================================
-
-function readable_markup($str)
-{
-    $str = str_replace("<","&lt;",$str);
-    $str = str_replace(">","&gt;",$str);
-    $str = str_replace("\n","<br />\n",$str);
-    return $str;
-}
-
-//================================================================================
-/*
 * Function simplify_html_tag
 *
 * This function is called by the simplify_html function or a site specific

3ffec22c9b104e2dd03fb08a481501983ae07e22
Andrew Perry <andy2@andperry.com>
2024-04-12 16:14:52 +0100

    Add Montserrat to available fonts

--- wp-content/themes/my-base-theme/shared/setup_params.php
+++ wp-content/themes/my-base-theme/shared/setup_params.php
@@ -65,6 +65,7 @@ if (!is_file($site_path_defs_path))
     // Create stylesheet links for the main site font
     $google_fonts = array (
         'Lato' => 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap',
+        'Montserrat' => 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap',
         'Noto Sans' => 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap',
         'Open Sans' => 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap',
         'Roboto' => 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',

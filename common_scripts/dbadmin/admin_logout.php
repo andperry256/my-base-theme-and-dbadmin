775c9597543d06cabc70b3ea743a29a8069e063c
Andrew Perry <andy2@andperry.com>
2024-07-23 13:22:18 +0100

    Add code to include shared_functions.php

--- common_scripts/dbadmin/admin_logout.php
+++ common_scripts/dbadmin/admin_logout.php
@@ -1,10 +1,20 @@
 <?php
 //==============================================================================
+//
+// N.B. The associated path_defs.php script must have been included by the 
+// calling script.
+//
+//==============================================================================
 
 if (session_status() ==  PHP_SESSION_NONE)
 {
     session_start();
 }
+if (!isset($base_dir))
+{
+    exit("Path definitions file not included.");
+}
+require("$base_dir/wp-content/themes/my-base-theme/shared_functions.php");
 $username = get_session_var(SV_USER);
 put_user('');
 if ($location == 'local')

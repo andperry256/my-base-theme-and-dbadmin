7312c8cad5220c02c8575de05e8e9dd693c96c72
Andrew Perry <andy2@andperry.com>
2024-07-24 09:39:35 +0100

    Add global declaration

--- common_scripts/allowed_hosts.php
+++ common_scripts/allowed_hosts.php
@@ -1,6 +1,7 @@
 <?php
 //==============================================================================
 
+global $allowed_hosts;
 require(__DIR__."/../../private_scripts/allowed_hosts.php");
 $allowed_hosts = array_merge(array('127.0.0.1' => 'Localhost'),$additional_allowed_hosts);
 

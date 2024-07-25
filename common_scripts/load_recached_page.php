64406dfc8b61300631aa1843bc6f368dd020c52a
Andrew Perry <andy2@andperry.com>
2024-07-25 11:55:27 +0100

    Add new file

---
+++ common_scripts/load_recached_page.php
@@ -0,0 +1,21 @@
+<?php
+//==============================================================================
+
+$tok1 = strtok(__DIR__,'/');
+$tok2 = strtok('/');
+$tok3 = strtok('/');
+$root_dir = "/$tok1/$tok2";
+if ($tok3 != 'public_html')
+{
+    // Extra directory level in special cases
+    $root_dir .= "/$tok3";
+}
+require("$root_dir/public_html/path_defs.php");
+require("$base_dir/wp-content/themes/my-base-theme/shared_functions.php");
+$uri_path = $_GET['uripath'];
+recache_page($uri_path);
+header("Location: $base_url/$uri_path/");
+exit;
+
+//==============================================================================
+?>

8e15592337c16f280a8c9a937ab6fa873f550afc
Andrew Perry <andy2@andperry.com>
2024-07-31 17:34:32 +0100

    Add code to optionally display database name

--- common_scripts/dbadmin/wp_db_scripts/actions/main.php
+++ common_scripts/dbadmin/wp_db_scripts/actions/main.php
@@ -1,12 +1,17 @@
 <?php
-  $db = admin_db_connect();
-  create_view_structure('_view_pages','wp_posts',"post_type='page' AND (post_status='draft' OR post_status='publish') ORDER BY post_name ASC");
-  create_view_structure('_view_posts','wp_posts',"post_type='post' AND (post_status='draft' OR post_status='publish') ORDER BY post_name ASC");
-  $set_fields = 'post_content';
-  $set_values = array('s','[No WordPress content - see custom scripts]');
-  $where_clause = "post_type='page' AND (post_content IS NULL OR post_content=''";
-  $where_values = array();
-  mysqli_update_query($db,'wp_posts',$set_fields,$set_values,$where_clause,$where_values);
+$dbname = (isset($_GET['dbname'])) ? $_GET['dbname'] : get_session_var('dbname');
+if (!empty($dbname))
+{
+  print("<p><strong>Database - <span class=\"highlight-success\">$dbname</span></strong></p>\n");
+}
+$db = admin_db_connect();
+create_view_structure('_view_pages','wp_posts',"post_type='page' AND (post_status='draft' OR post_status='publish') ORDER BY post_name ASC");
+create_view_structure('_view_posts','wp_posts',"post_type='post' AND (post_status='draft' OR post_status='publish') ORDER BY post_name ASC");
+$set_fields = 'post_content';
+$set_values = array('s','[No WordPress content - see custom scripts]');
+$where_clause = "post_type='page' AND (post_content IS NULL OR post_content=''";
+$where_values = array();
+mysqli_update_query($db,'wp_posts',$set_fields,$set_values,$where_clause,$where_values);
 ?>
 <p><strong>WARNING</strong> - You are editing the main WordPress database. Please do so with caution!!<p>
   <ul>

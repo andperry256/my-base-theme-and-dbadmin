9040cec666848a8b28cafc96df3eea264fc2afec
Andrew Perry <andy2@andperry.com>
2024-09-03 20:14:24 +0100

    Add new files

---
+++ common_scripts/display_table.php
@@ -0,0 +1,139 @@
+<?php
+//==============================================================================
+
+// Load paths and validate parameters
+if (is_file('/Config/linux_pathdefs.php'))
+{
+    $local_site_dir = strtok(substr($_SERVER['REQUEST_URI'],1),'/');
+}
+require("{$_SERVER['DOCUMENT_ROOT']}/path_defs.php");
+require("$base_dir/common_scripts/dbadmin/table_funct.php");
+require("$base_dir/common_scripts/dbadmin/record_funct.php");
+if (empty($_GET['dbname']))
+{
+    exit("Database not specified");
+}
+$dbname = $_GET['dbname'];
+if (empty($_GET['table']))
+{
+    exit("Table not specified");
+}
+$table = $_GET['table'];
+require("$base_dir/common_scripts/session_funct.php");
+run_session();
+if (empty(get_session_var(array('dbauth',$dbname))))
+{
+    exit("Authentication failure");
+}
+if (!is_file("$base_dir/non_wp_header.php"))
+{
+    exit("Non-WP header file not found");
+}
+require("$base_dir/non_wp_header.php");
+
+//==============================================================================
+?>
+<style>
+    a {
+        color:#fff !important;
+        text-decoration: none !important;
+    }
+    .table-header {
+        background-color: #7f7f7f;
+        color: #fff;
+    }
+</style>
+<?php
+//==============================================================================
+
+// Connect to database
+foreach ($dbinfo as $id => $data)
+{
+    if ((($location == 'local') && ($data[0] == $dbname)) ||
+        (($location == 'real') && ($data[1] == $dbname)))
+    {
+        $db = db_connect($id);
+        break;
+    }
+}
+if (empty($db))
+{
+    exit("Unable to connect to database");
+}
+
+print("<h2>Table [$table]</h2>\n");
+
+// Build lists of primary keys and fields to display
+$base_table = get_base_table($table,$db);
+$display_fields = array();
+$pklist = '';
+$query_result = mysqli_query_normal($db,"SHOW COLUMNS FROM $table");
+while ($row = mysqli_fetch_assoc($query_result))
+{
+    $where_clause = 'table_name=? AND field_name=?';
+    $where_values = array('s',$base_table,'s',$row['Field']);
+    if ($row2 = mysqli_fetch_assoc(mysqli_select_query($db,'dba_table_fields','*',$where_clause,$where_values,'')))
+    {
+        if ($row2['is_primary'])
+        {
+            $pklist .= "{$row2['field_name']} ASC,";
+        }
+        if ($row2['list_desktop'] ==  1)
+        {
+            $display_fields[$row2['display_order']] = $row2['field_name'];
+        }
+    }
+}
+$excluded_fields = (isset($_GET['excluded'])) ? $_GET['excluded'] : '^';
+
+// Output the table
+$pklist = rtrim($pklist,',');
+if (empty($pklist))
+{
+    exit("Table has no primary key(s)");
+}
+
+print("<table>\n");
+print("<tr>");
+foreach ($display_fields as $order => $field)
+{
+    if (strpos($excluded_fields,"^$field^") === false)
+    {
+        $label = field_label($base_table,$field,$db);
+        $excluded_fields_par=(urlencode("$excluded_fields$field^"));
+        print("<td class=\"table-header\">");
+        print("<a href=\"./display_table.php?dbname=$dbname&table=$table&excluded=$excluded_fields_par\">$label</a></td>");
+    }
+}
+print("</tr>\n");
+
+$add_clause = "ORDER BY $pklist";
+$query_result = mysqli_select_query($db,$base_table,'*','',array(),$add_clause);
+while ($row = mysqli_fetch_assoc($query_result))
+{
+    print("<tr>");
+    foreach ($display_fields as $order => $field)
+    {
+        if (strpos($excluded_fields,"^$field^") === false)
+        {
+            $where_clause = 'field_name=?';
+            $where_values = array('s',$field);
+            if (($row2 = mysqli_fetch_assoc(mysqli_select_query($db,'dba_table_fields','*',$where_clause,$where_values,''))) && 
+                ($row2['widget_type'] == 'checkbox'))
+            {
+                $value = ($row[$field]) ? '[X]' : '';
+            }
+            else
+            {
+                $value = $row[$field];
+            }
+            print("<td>$value</td>");        
+        }
+    }
+    print("</tr>\n");
+}
+
+print("</table>\n");
+
+//==============================================================================
+?>
---
+++ common_scripts/network_scan_results.php
@@ -0,0 +1,108 @@
+<?php
+//==============================================================================
+
+$local_site_dir = strtok(substr($_SERVER['REQUEST_URI'],1),'/');
+require("{$_SERVER['DOCUMENT_ROOT']}/path_defs.php");
+require("$base_dir/non_wp_header.php");
+$db = local_itservices_db_connect();
+
+$mac_addresses = array();
+$vendors = array();
+$content = file_get_contents($network_scan_output);
+
+// Loop through all host specifications in the XML script
+$pos1 = strpos($content,'<host>');
+while ($pos1 !== false)
+{
+    // Extract data for given host
+    $pos2 = strpos($content,'</host>',$pos1);
+    $pos3 = strpos($content,'<',$pos2);
+    $host_data = explode("\n",substr($content,$pos1,$pos3-$pos1));
+    $up = false;
+    $ip_address = '';
+    $mac_address = '';
+    $vendor = '';
+    foreach($host_data as $line)
+    {
+        if (strpos($line,'status state="up"') !== false)
+        {
+            $up = true;
+        }
+        elseif (strpos($line,'addrtype="ipv4"') !== false)
+        {
+            $pos4 = strpos($line,'addr=') + 6;
+            $pos5 = strpos($line,'"',$pos4);
+            $ip_address = substr($line,$pos4,$pos5-$pos4);
+        }
+        elseif (strpos($line,'addrtype="mac"') !== false)
+        {
+            $pos4 = strpos($line,'addr=') + 6;
+            $pos5 = strpos($line,'"',$pos4);
+            $mac_address = substr($line,$pos4,$pos5-$pos4);
+            if (strpos($line,'vendor=') !== false)
+            {
+                $pos4 = strpos($line,'vendor=') + 8;
+                $pos5 = strpos($line,'"',$pos4);
+                $vendor = substr($line,$pos4,$pos5-$pos4);
+            }
+        }
+        if (($up) && (!empty($ip_address)))
+        {
+            $mac_addresses[$ip_address] = $mac_address;
+            $vendors[$ip_address] = $vendor;
+        }
+    }
+    $pos1 = strpos($content,'<host>',$pos3);
+}
+
+print("<table>\n");
+print("<tr><td>IP Address</td><td>Name</td><td>MAC Address</td><td>Vendor</td></tr>\n");
+
+// Loop through all possible IP addresses on the network
+for ($sub_addr=1; $sub_addr<=254; $sub_addr++)
+{
+    $ip_address = "$ip_subnet_addr.$sub_addr";
+    if (!empty($mac_addresses[$ip_address]))
+    {
+        // IP address found on scan (online)
+        $mac_address = $mac_addresses[$ip_address];
+        $where_clause = 'mac_address=?';
+        $where_values = array('s',$mac_addresses[$ip_address]);
+        if ($row = mysqli_fetch_assoc(mysqli_select_query($db,'home_dhcp_settings','*',$where_clause,$where_values,'')))
+        {
+            // Device matches item in DHCP settings table
+            $name = (empty($row['friendly_name'])) ? $row['hostname'] : $row['friendly_name'];
+        }
+        else
+        {
+            // No match in DHCP settings table
+            $name = '';
+        }
+        $vendor = $vendors[$ip_address];
+        $status = 'online';
+    }
+    elseif (($where_clause = 'ip_element_4=?') &&
+            ($where_values = array('i',$sub_addr)) && 
+            ($row = mysqli_fetch_assoc(mysqli_select_query($db,'home_dhcp_settings','*',$where_clause,$where_values,''))))
+    {
+        // Item in DHCP settings table not found on scan (offline)
+        $mac_address = $row['mac_address'];
+        $name = (empty($row['friendly_name'])) ? $row['hostname'] : $row['friendly_name'];
+        $vendor = '';
+        $status = 'offline';
+    }
+    else
+    {
+        $status = '';
+    }
+    if (!empty($status))
+    {
+        print("<tr><td class=\"td-$status\">$ip_address</td><td class=\"td-$status\">$name</td><td class=\"td-$status\">$mac_address</td><td class=\"td-$status\">$vendor</td></tr>");
+        print("\n");
+    }
+}
+
+print("</table>\n");
+
+//==============================================================================
+?>

af96ba6bece99147d27e7c499f994af0ff66574c
Andrew Perry <andy2@andperry.com>
2025-01-10 18:44:05 +0000

    Dummy out suspected redundant code

--- common_scripts/allowed_hosts.php
+++ common_scripts/allowed_hosts.php
@@ -4,9 +4,9 @@
 global $allowed_hosts, $location;
 require(__DIR__."/../../private_scripts/allowed_hosts.php");
 $allowed_hosts = array_merge(array('127.0.0.1' => 'Localhost'),$additional_allowed_hosts);
-if (($location == 'real') && (!isset($allowed_hosts[$_SERVER['REMOTE_ADDR']])))
+if (false) // (($location == 'real') && (!isset($allowed_hosts[$_SERVER['REMOTE_ADDR']])))
 {
-    // exit("Authentication failure");
+    exit("Authentication failure");
 }
 
 //==============================================================================

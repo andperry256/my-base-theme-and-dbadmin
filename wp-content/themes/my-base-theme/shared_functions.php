b451dc864452709d27a04e40bd3dae70a5d38910
Andrew Perry <andy2@andperry.com>
2025-05-23 09:28:14 +0100

    Move font info data into separate script

--- wp-content/themes/my-base-theme/shared_functions.php
+++ wp-content/themes/my-base-theme/shared_functions.php
@@ -251,20 +251,7 @@ function output_font_stylesheet_links()
     $default_header_font = 'Roboto';
     $mono_elements = 'pre, code, textarea';
     $default_mono_font = 'Source Code Pro';
-
-    // List of available fonts.
-    $main_fonts = array (
-        'IBM Plex Sans' => 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
-        'Lato' => 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap',
-        'Montserrat' => 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap',
-        'Noto Sans' => 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap',
-        'Open Sans' => 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap',
-        'Roboto' => 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
-    );
-    $mono_fonts = array (
-        'IBM Plex Mono' => 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
-        'Source Code Pro' => 'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap',
-    );
+    require(__DIR__.'/font_lists.php');
 
     // Set up variables for main, header and mono fonts.
     if ((!isset($main_font)) || (!isset($main_fonts[$main_font])))

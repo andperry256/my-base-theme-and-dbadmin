c7237bb5236058dce56d2906eed5dab3a4df16fd
Andrew Perry <andy2@andperry.com>
2025-07-28 10:27:41 +0100

    Add new file

---
+++ common_scripts/wp-config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Generic script for all sites. This needs to be copied into public_html.
+The main functionality for the individual site is contained in:
+
+<root dir>/private_scripts/wp-config.php
+*/
+require(__DIR__.'/../private_scripts/wp-config.php');
+
+/** Absolute path to the WordPress directory. **/
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. **/
+require_once ABSPATH . 'wp-settings.php';
+?>

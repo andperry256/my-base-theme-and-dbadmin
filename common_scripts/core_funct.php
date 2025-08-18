2328abf3fabaaa904337699bf987b011ba697046
Andrew Perry <andy2@andperry.com>
2025-08-18 11:32:26 +0100

    Add function get_url_content
    Also need to add check that script is only executed once.

--- common_scripts/core_funct.php
+++ common_scripts/core_funct.php
@@ -1,5 +1,7 @@
 <?php
 //==============================================================================
+if (!defined('CORE_FUNCT_DEFINED')):
+//==============================================================================
 
 if ((isset($base_dir)) && (isset($private_scripts_dir))) {
     require_once("$base_dir/mysql_connect.php");
@@ -10,4 +12,31 @@ else {
     exit("Directory paths not found\n");
 }
 
+//==============================================================================
+/*
+Function get_url_content
+*/
+//==============================================================================
+
+function get_url_content($url,$debug=false)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $content = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $content = 'cURL Error: '.curl_error($ch);
+    }
+    if ($debug) {
+        exit("$content\n");
+    }
+    else {
+        curl_close($ch);
+        return $content;
+    }
+}
+
+//==============================================================================
+define('CORE_FUNCT_DEFINED',true);
+endif;
 //==============================================================================

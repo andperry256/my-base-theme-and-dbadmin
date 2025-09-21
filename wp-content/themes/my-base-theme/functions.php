434cfcf4a9577c21a16983cde0ef3bad8f136f38
Andrew Perry <andy2@andperry.com>
2025-09-21 18:59:49 +0100

    Add condition to calling wp_enqueue_style
    To not be called when there is a child theme present.

--- wp-content/themes/my-base-theme/functions.php
+++ wp-content/themes/my-base-theme/functions.php
@@ -160,19 +160,21 @@ add_action( 'widgets_init', 'my_base_theme_widgets_init' );
  */
 function my_base_theme_scripts()
 {
-    global $link_version;
-    if (!isset($link_version)) {
-        $link_version = date('ym').'01';
+    global $last_preset_link_version;
+    if (!isset($last_preset_link_version)) {
+        $last_preset_link_version = date('ym').'01';
     }
     $stylesheet_uri = get_stylesheet_uri();
     if (function_exists('url_to_static')) {
         $stylesheet_uri = url_to_static($stylesheet_uri);
     }
-    wp_enqueue_style( 'my-base-theme-style', $stylesheet_uri, '', $link_version );
+    if (!is_child_theme()) {
+        wp_enqueue_style( 'my-base-theme-style', $stylesheet_uri, '', $last_preset_link_version );
+    }
 
-    wp_enqueue_script( 'my-base-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+    wp_enqueue_script( 'my-base-theme-navigation', get_template_directory_uri() . '/js/navigation.js', [], $last_preset_link_version, true );
 
-    wp_enqueue_script( 'my-base-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script( 'my-base-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', [], $last_preset_link_version, true );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );

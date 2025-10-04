c4e93dea287258d624b35c8debf6ea031bda8593
Andrew Perry <andy2@andperry.com>
2025-10-04 19:41:41 +0100

    Minor formatting changes

--- common_scripts/dbadmin/finance_db_scripts/actions/delete_unused_payees_2.php
+++ common_scripts/dbadmin/finance_db_scripts/actions/delete_unused_payees_2.php
@@ -5,13 +5,12 @@ $db = admin_db_connect();
 print("<h1>Delete Unused Payees</h1>\n");
 $where_clause = "instances=0 AND (default_fund IS NULL OR default_fund='') AND ";
 $where_clause .= "(default_cat IS NULL OR default_cat='') AND name NOT LIKE '**%'";
-$where_values = [];
 $add_clause = 'ORDER BY name ASC';
 $query_result = mysqli_select_query($db,'payees','*',$where_clause,[],$add_clause);
 while ($row = mysqli_fetch_assoc($query_result)) {
-    print("<p>Payee <em>{$row['name']}</em> deleted.</p>\n");
+    print("Payee <em>{$row['name']}</em> deleted.<br />\n");
 }
-mysqli_delete_query($db,'payees',$where_clause,$where_values);
+mysqli_delete_query($db,'payees',$where_clause,[]);
 print("<p>Operation completed.</p>\n");
 
 //==============================================================================

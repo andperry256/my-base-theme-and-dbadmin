4178a8713121880752bfe09c849ab55343b45d00
Andrew Perry <andy2@andperry.com>
2025-10-04 19:41:28 +0100

    Update to use count_payee_instances function

--- common_scripts/dbadmin/finance_db_scripts/actions/delete_unused_payees_1.php
+++ common_scripts/dbadmin/finance_db_scripts/actions/delete_unused_payees_1.php
@@ -5,22 +5,13 @@ $db = admin_db_connect();
 print("<h1>Delete Unused Payees</h1>\n");
 print("<p>The following payees are currently unused and will be deleted:-</p>\n");
 print("<ul>\n");
+count_payee_instances();
+$where_clause = "instances=0 AND (default_fund IS NULL OR default_fund='') AND ";
+$where_clause .= "(default_cat IS NULL OR default_cat='') AND name NOT LIKE '**%'";
 $add_clause = ' ORDER BY name ASC';
-$query_result = mysqli_select_query($db,'payees','*','',[],$add_clause);
+$query_result = mysqli_select_query($db,'payees','*',$where_clause,[],$add_clause);
 while ($row = mysqli_fetch_assoc($query_result)) {
-      $payee = $row['name'];
-      $where_clause = 'payee=?';
-      $where_values = ['s',$payee];
-      $query_result2 = mysqli_select_query($db,'transactions','*',$where_clause,$where_values,'');
-      $count = mysqli_num_rows($query_result2);
-      $set_fields = 'instances';
-      $set_values = ['i',$count];
-      $where_clause = 'name=?';
-      $where_values = ['s',$payee];
-      mysqli_update_query($db,'payees',$set_fields,$set_values,$where_clause,$where_values);
-      if (($count == 0) && (empty($row['default_fund'])) && (empty($row['default_cat'])) && (substr($row['name'],0,2) != '**')) {
-          print("<li>{$row['name']}</li>\n");
-      }
+    print("<li>{$row['name']}</li>\n");
 }
 print("</ul>\n");
 print("<p><a href=\"index.php?-action=delete_unused_payees_2\"><button>Continue</button></a></p>\n");

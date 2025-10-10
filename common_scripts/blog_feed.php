909ce27409bfbaa23c184c36dd66eed543fc1b56
Andrew Perry <andy2@andperry.com>
2025-10-10 15:02:38 +0100

    Add check on post access level

--- common_scripts/blog_feed.php
+++ common_scripts/blog_feed.php
@@ -30,40 +30,49 @@ function update_blog_feed($title,$description)
 
     // Main loop to process posts.
     $where_clause = "post_type='post' AND post_status='publish'";
-    $add_clause = "ORDER BY post_date DESC LIMIT 30";
+    $add_clause = "ORDER BY post_date DESC";
     $query_result = mysqli_select_query($db,'wp_posts','*',$where_clause,[],$add_clause);
+    $count = 0;
     while ($row = mysqli_fetch_assoc($query_result)) {
-        fprintf($ofp,"  <item>\n");
-        $title_par = str_replace('%','%%',$row['post_title']);
-        fprintf($ofp,"    <title>$title_par</title>\n");
-        fprintf($ofp,"    <link>$base_url/{$row['post_name']}</link>\n");
+        $post_access_level = get_post_meta($row['ID'],'access_level',true);
+        if (($post_access_level !== '') && (defined('DEFAULT_ACCESS_LEVEL')) && ($post_access_level > DEFAULT_ACCESS_LEVEL)) {
+            // Omit post above default access level.
+        }
+        else {
+            fprintf($ofp,"  <item>\n");
+            $title_par = str_replace('%','%%',$row['post_title']);
+            fprintf($ofp,"    <title>$title_par</title>\n");
+            fprintf($ofp,"    <link>$base_url/{$row['post_name']}</link>\n");
 
-        // Format description, including featured image where available.
-        fprintf($ofp,"    <description>\n");
-        fprintf($ofp,"      <![CDATA[\n");
-        fprintf($ofp,"        <p>$title_par</p>\n");
-        $where_clause = "post_id=? AND meta_key='_thumbnail_id'";
-        $where_values = ['i',$row['ID']];
-        if ($row2 = mysqli_fetch_assoc(mysqli_select_query($db,'wp_postmeta','meta_value',$where_clause,$where_values,''))) {
-            $where_clause = "ID=? AND post_type='attachment'";
-            $where_values = ['i',$row2['meta_value']];
-            if ($row3 = mysqli_fetch_assoc(mysqli_select_query($db,'wp_posts','guid',$where_clause,$where_values,''))) {
-                $image_url = get_modified_image_url($row3['guid'],'webp192','webp');
-                if (!empty($image_url)) {
-                    fprintf($ofp,"        <img src=\"$image_url\" />\n");
+            // Format description, including featured image where available.
+            fprintf($ofp,"    <description>\n");
+            fprintf($ofp,"      <![CDATA[\n");
+            fprintf($ofp,"        <p>$title_par</p>\n");
+            $where_clause = "post_id=? AND meta_key='_thumbnail_id'";
+            $where_values = ['i',$row['ID']];
+            if ($row2 = mysqli_fetch_assoc(mysqli_select_query($db,'wp_postmeta','meta_value',$where_clause,$where_values,''))) {
+                $where_clause = "ID=? AND post_type='attachment'";
+                $where_values = ['i',$row2['meta_value']];
+                if ($row3 = mysqli_fetch_assoc(mysqli_select_query($db,'wp_posts','guid',$where_clause,$where_values,''))) {
+                    $image_url = get_modified_image_url($row3['guid'],'webp192','webp');
+                    if (!empty($image_url)) {
+                        fprintf($ofp,"        <img src=\"$image_url\" />\n");
+                    }
                 }
             }
+            fprintf($ofp,"      ]]>\n");
+            fprintf($ofp,"    </description>\n");
+
+            // Format and include publication date.
+            $date = date('D, d M Y H:i:s',strtotime($row['post_date_gmt'])).' GMT';
+            fprintf($ofp,"    <pubDate>$date</pubDate>\n");
+
+            fprintf($ofp,"  </item>\n");
+            $count++;
         }
-        fprintf($ofp,"      ]]>\n");
-        fprintf($ofp,"    </description>\n");
-
-        // Format and include publication date.
-        $date = date('D, d M Y H:i:s',strtotime($row['post_date_gmt'])).' GMT';
-        fprintf($ofp,"    <pubDate>$date</pubDate>\n");
-
-        // Find and include featured image if present.
-
-        fprintf($ofp,"  </item>\n");
+        if ($count >= 30) {
+            break;
+        }
     }
 
     // Output footer lines.

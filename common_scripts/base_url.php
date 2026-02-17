d68bd38028084b9a69bc85a652332e1a411892df
Andrew Perry <andy2@andperry.com>
2026-02-17 12:15:51 +0000

    Correct included script path

--- common_scripts/base_url.php
+++ common_scripts/base_url.php
@@ -1,3 +1,8 @@
 <?php
-require(__DIR__."/../path_defs.php");
-exit($base_url);
+if (isset($argc)) {
+    include(__DIR__.'/include_pathdefs.php');
+    exit($base_url);
+}
+else {
+    // Do not allow in web mode.
+}

7174c09c95804192be9710eae9b553673ed46a47
Andrew Perry <andy2@andperry.com>
2026-02-27 18:57:08 +0000

    Various changes
    1. Simplify inclusion of path_defs.php.
    2. Add user message and link to home.

--- common_scripts/user_logout.php
+++ common_scripts/user_logout.php
@@ -4,18 +4,12 @@
 if (session_status() ==  PHP_SESSION_NONE) {
     session_start();
 }
-if (is_file('/Config/linux_pathdefs.php')) {
-    // Local server
-    $subdir = strtok(ltrim($_SERVER['REQUEST_URI'],'/'),'/');
-    include("{$_SERVER['DOCUMENT_ROOT']}/{$subdir}/path_defs.php");
-}
-else {
-    // Online server
-    include("{$_SERVER['DOCUMENT_ROOT']}/path_defs.php");
-}
+include("{$_SERVER['DOCUMENT_ROOT']}/path_defs.php");
 require("$base_dir/wp-content/themes/my-base-theme/shared_functions.php");
 $username = $_SESSION[SV_USER];
 put_user('');
+print("<p>User logged out</p>\n");
+print("<p><a href=\"$base_url\">Home</a></p>\n");
 exit;
 
 //==============================================================================

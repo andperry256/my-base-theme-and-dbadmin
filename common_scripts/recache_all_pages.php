64e7a2af2bb2b388c31cb1d370e0eaca624b3cbe
Andrew Perry <andy2@andperry.com>
2026-03-07 13:39:58 +0000

    Simplify path determination

--- common_scripts/recache_all_pages.php
+++ common_scripts/recache_all_pages.php
@@ -7,25 +7,14 @@
 */
 //==============================================================================
 
-$tok1 = strtok(__DIR__,'/');
-if ($tok1 == 'home') {
-    $tok2 = strtok('/');
-    $tok3 = strtok('/');
-    $root_dir = "/$tok1/$tok2";
-    if ($tok3 != 'public_html') {
-        // Extra directory level in special cases
-        $root_dir .= "/$tok3";
-    }
-    require("$root_dir/public_html/path_defs.php");
+require_once(__DIR__.'/get_local_site_dir.php');
+if ($location == 'real') {
     require("$base_dir/wp-content/themes/my-base-theme/shared_functions.php");
     recache_all_pages('page');
     recache_all_pages('post');
     if (function_exists('recache_additional_pages')) {
         recache_additional_pages();
     }
-}
-else {
-    // Invalid environment - exit with no action.
 }
 
 //==============================================================================

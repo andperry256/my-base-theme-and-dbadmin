c5e09f8ad9a90d7f4e3f95b1e27173abfdbe1a3a
Andrew Perry <andy2@andperry.com>
2026-06-12 12:32:56 +0100

    Make script usable outside a website-specific context

--- common_scripts/core_funct.php
+++ common_scripts/core_funct.php
@@ -4,38 +4,42 @@ if (!defined('CORE_FUNCT_DEFINED')):
 //==============================================================================
 
 if ((isset($base_dir)) && (isset($private_scripts_dir))) {
+
+    // Website specific functionality
     require_once("$base_dir/mysql_connect.php");
-    require_once("$base_dir/common_scripts/mysql_funct.php");
     require_once("$base_dir/common_scripts/session_funct.php");
-}
-else {
-    exit("Directory paths not found\n");
+
+    //==============================================================================
+    /*
+    Function get_url_content
+    */
+    //==============================================================================
+
+    function get_url_content($url,$debug=false)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        $content = curl_exec($ch);
+        if (curl_errno($ch)) {
+            $content = 'cURL Error: '.curl_error($ch);
+        }
+        if ($debug) {
+            exit("$content\n");
+        }
+        else {
+            curl_close($ch);
+            return $content;
+        }
+    }
 }
 
-//==============================================================================
 /*
-Function get_url_content
+The following functionality is not website specific and may be used in general
+system utilities.
 */
-//==============================================================================
-
-function get_url_content($url,$debug=false)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-    $content = curl_exec($ch);
-    if (curl_errno($ch)) {
-        $content = 'cURL Error: '.curl_error($ch);
-    }
-    if ($debug) {
-        exit("$content\n");
-    }
-    else {
-        curl_close($ch);
-        return $content;
-    }
-}
+require_once(__DIR__."/mysql_funct.php");
 
 //==============================================================================
 /*

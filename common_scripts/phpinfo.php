19e092c85a229d795a1c4c754d1a21081bcf2ee8
Andrew Perry <andy2@andperry.com>
2026-06-18 20:14:30 +0100

    Add code for listing PHP extenstions

--- common_scripts/phpinfo.php
+++ common_scripts/phpinfo.php
@@ -10,5 +10,13 @@ if ((!isset($allowed_hosts[$_SERVER['REMOTE_ADDR']])) && (!is_local_ip($_SERVER[
     exit("Authentication failure");
 }
 phpinfo();
+print("<p style=\"text-align:center; font-size:150%; font-weight:bold\">Loaded Extensions</strong></p>\n");
+print("<div style=\"display:block; width:890px; background-color: #ffe; border:solid 1px #666; padding:20px; margin:1.0em auto\">\n");
+$extensions = get_loaded_extensions();
+usort($extensions, 'strcasecmp');
+foreach($extensions as $extension) {
+    print("$extension<br />\n");
+}
+print("</div>\n");
 
 //==============================================================================

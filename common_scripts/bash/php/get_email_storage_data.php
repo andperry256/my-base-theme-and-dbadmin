e54d94a485e6984ad147d78db5f64cb9eb9ecdc1
Andrew Perry <andy2@andperry.com>
2026-07-17 11:46:29 +0100

    Update to work with new table structure
    ... where  the site path is stored

--- common_scripts/bash/php/get_email_storage_data.php
+++ common_scripts/bash/php/get_email_storage_data.php
@@ -1,5 +1,13 @@
 <?php
 //==============================================================================
+/*
+This script is called to determine the used storage for those email mailboxes on
+the current site which are listed for checking. It exits as an output string of
+format:
+
+<domain>+<mailbox>+<storage>+<domain>+<mailbox>+<storage> ...
+*/
+//==============================================================================
 
 if ((PHP_SAPI !== 'cli') && (PHP_SAPI !== 'cli-fcgi')) {
     exit("Script valid in command line mode only\n");
@@ -7,23 +15,30 @@ if ((PHP_SAPI !== 'cli') && (PHP_SAPI !== 'cli-fcgi')) {
 elseif (substr(__DIR__,0,5) != '/home') {
     exit("Script valid on online server only\n");
 }
-// Parent directory hierarchy: bash => common_scripts => public_html
+// Parent directory hierarchy: => bash => common_scripts => public_html
 require(__DIR__."/../../../path_defs.php");
-$domain = $argv[1];
-$mailbox_list = explode('+',$argv[2]);
 $mailbox_data = '';
-foreach ($mailbox_list as $mailbox) {
-    $content = file("$root_dir/mail/$domain/$mailbox/dovecot-quota");
-    if (empty($content)) {
-        print("Email account $mailbox@$domain not found\n");
+if ((function_exists('online_itservices_db_connect')) && ($db = itservices_db_connect())) {
+    $site_path = $argv[1];
+    $where_clause = 'site_path=?';
+    $where_values = ['s',$site_path];
+    $add_clause = 'ORDER BY domain ASC, mailbox ASC';
+    $query_result = mysqli_select_query($db,'email_storage','*',$where_clause,$where_values,$add_clause);
+    while ($row = mysqli_fetch_assoc($query_result)) {
+        $content = file("$root_dir/mail/{$row['domain']}/{$row['mailbox']}/dovecot-quota");
+        if (empty($content)) {
+            print("Email account {$row['mailbox']}@{$row['domain']} not found\n");
+        }
+        else {
+            // Add the domain, mailbox and used storage to the mailbox data string,
+            // separated with the delimeter '+'.
+            $last_line = $content[count($content) -1];
+            $used_storage = trim($last_line);
+            $mailbox_data .= "{$row['domain']}+{$row['mailbox']}+{$used_storage}+";
+        }
     }
-    else {
-        $last_line = $content[count($content) -1];
-        $used_storage = trim($last_line);
-        $mailbox_data .= $mailbox.'+'.$used_storage.'+';
-    }
+    $mailbox_data = rtrim($mailbox_data,'+');
 }
-$mailbox_data = rtrim($mailbox_data,'+');
 exit($mailbox_data);
 
 //==============================================================================

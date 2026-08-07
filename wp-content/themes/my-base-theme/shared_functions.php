1a83aae1e21881f7ad7444c4303eedbbf326500b
Andrew Perry <andy2@andperry.com>
2026-08-07 11:30:43 +0100

    Add option for no counter maintained

--- wp-content/themes/my-base-theme/shared_functions.php
+++ wp-content/themes/my-base-theme/shared_functions.php
@@ -1362,26 +1362,30 @@ function display_web_counter($dbid,$db=null)
     if (empty($db)) {
         $db = db_connect($dbid);
     }
-    $end_year = current_counter_year($dbid,$db);
-    $today_date = date('Y-m-d');
-    $today_count = mysqli_num_rows(mysqli_query($db,"SELECT * FROM counter_hits WHERE date='$today_date'"));
-    $counter_value = ($row = mysqli_fetch_assoc(mysqli_query($db,"SELECT * FROM counter_info WHERE id='{$end_year}_count'")))
-        ? $row['value']
-        : null;
-    $daily_average = ($row = mysqli_fetch_assoc(mysqli_query($db,"SELECT * FROM counter_info WHERE id='{$end_year}_daily'")))
-        ? $row['value']
-        : null;
-    $start_date = ($row = mysqli_fetch_assoc(mysqli_query($db,"SELECT * FROM counter_info WHERE id='{$end_year}_start'")))
-        ? $row['value']
-        : null;
-    $start_date = date('d M Y',strtotime($start_date));
-    $start_date = str_replace(' ','&nbsp;',$start_date);
-    print("<p style=\"line-height:1.6em\">");
-    print("Count: &nbsp;$counter_value<br />");
-    print("Since: &nbsp;$start_date<br />");
-    print(sprintf("Daily: &nbsp;%01.1f<br />",$daily_average));
-    print("Today: &nbsp;$today_count</p>\n");
-
+    if (mysqli_num_rows(mysqli_query($db,"SELECT * FROM counter_info WHERE id='start_month'")) > 0) {
+        $end_year = current_counter_year($dbid,$db);
+        $today_date = date('Y-m-d');
+        $today_count = mysqli_num_rows(mysqli_query($db,"SELECT * FROM counter_hits WHERE date='$today_date'"));
+        $counter_value = ($row = mysqli_fetch_assoc(mysqli_query($db,"SELECT * FROM counter_info WHERE id='{$end_year}_count'")))
+            ? $row['value']
+            : null;
+        $daily_average = ($row = mysqli_fetch_assoc(mysqli_query($db,"SELECT * FROM counter_info WHERE id='{$end_year}_daily'")))
+            ? $row['value']
+            : null;
+        $start_date = ($row = mysqli_fetch_assoc(mysqli_query($db,"SELECT * FROM counter_info WHERE id='{$end_year}_start'")))
+            ? $row['value']
+            : null;
+        $start_date = date('d M Y',strtotime($start_date));
+        $start_date = str_replace(' ','&nbsp;',$start_date);
+        print("<p style=\"line-height:1.6em\">");
+        print("Count: &nbsp;$counter_value<br />");
+        print("Since: &nbsp;$start_date<br />");
+        print(sprintf("Daily: &nbsp;%01.1f<br />",$daily_average));
+        print("Today: &nbsp;$today_count</p>\n");
+    }
+    else {
+        print("<p>No counter maintained</p>");
+    }
 }
 
 //==============================================================================

415ae5710e4ae5ac8b4d90113f6564f02a13e308
Andrew Perry <andy2@andperry.com>
2026-08-10 21:56:46 +0100

    Implement new functionality
    To operate in public_html/media directory in conjunction with externally stored files.

--- common_scripts/configure_linked_dirs.php
+++ common_scripts/configure_linked_dirs.php
@@ -13,40 +13,25 @@ if (!isset($base_dir)) {
     exit("Path definitions file not found");
 }
 require_once("$base_dir/keycode.php");
-
 $mode = php_server_mode();
 $eol = eol_string();
 
-$rewrite_rules = [];
-foreach ($dirs as $dir) {
-    print("$eol*** Processing $dir ***$eol");
-    $dir_path = "$files_subdomain_dir/$dir";
-    $dirlist = scandir($dir_path);
-    foreach ($dirlist as $file) {
-        if (substr($file,0,8)  == 'storage-') {
-            $storage_dir = $file;
-            break;
-        }
-    }
-    if ($storage_dir == 'storage-000000') {
-        // Local server
-        $links_path = 'links-000000';
-    }
-    else {
-        // Online site
-        $key = password_hash($key,PASSWORD_DEFAULT);
-        $key = md5($key);
-        $key = substr($key,0,32);
-        $links_path = "links-$key";
-    }
-    $content = file_get_contents("$dir_path/paths.php");
-    $content = preg_replace('/links-[0-9a-f]+/',"$links_path",$content);
-    file_put_contents("$dir_path/paths.php",$content);
-    $ofp = fopen("$dir_path/.htaccess",'w');
-    fprintf($ofp,"RewriteEngine On\nRewriteRule ^$links_path/(.*)\$ $storage_dir/\$1\n");
-    fclose($ofp);
-    print("Links directory is now $links_path$eol");
+if ($location == 'local') {
+    $links_path = 'links-000000';
 }
+else {
+    $key = password_hash($key,PASSWORD_DEFAULT);
+    $key = md5($key);
+    $key = substr($key,0,32);
+    $links_path = "links-$key";
+}
+
+foreach (['paths.php','.htaccess'] as $file) {
+    $content = file_get_contents("$base_dir/media/$file");
+    $content = preg_replace('/links-[0-9a-f]+/',"$links_path",$content);
+    file_put_contents("$base_dir/media/$file",$content);
+}
+print("Links URL is now $base_url/media/$links_path$eol");
 print("Operation completed$eol");
 
 //==============================================================================
